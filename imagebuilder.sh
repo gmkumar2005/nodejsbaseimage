626135ba99f6e871d68af84057a6f74f8d9bc6be
kiran <gmkumar2005@gmail.com>
2020-05-05 13:47:10 +0530

    ng build with pwd

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -442,9 +442,11 @@ buildah config --workingdir /app  $buildcntr2
 buildah config --env NG_CLI_ANALYTICS=ci $buildcntr2 
 buildah config --env PATH=/app/node_modules/.bin:$PATH $buildcntr2
 info "Start yarn"
-buildah run $buildcntr2 -- yarn install  --silent --non-interactive   
+buildah run $buildcntr2 -- yarn install  --silent --non-interactive 
+buildah run $buildcntr2 -- yarn --silent --non-interactive global add @angular/cli   
 buildah run $buildcntr2 -- yarn cache clean --silent --non-interactive
-buildah run $buildcntr2 -- ng build --output-path=/var/www/html 
+# buildah run $buildcntr2 -- ng build --output-path=/var/www/html 
+buildah run $buildcntr2 -- sh -c "(pwd && ng build --output-path=/var/www/html)"
 info "ng build completed"
 info "Remove default server definition"
 buildah run $buildcntr2 rm /etc/nginx/conf.d/default.conf
