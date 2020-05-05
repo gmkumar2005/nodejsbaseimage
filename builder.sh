6042c1f3004f3ac89ab96a3559a26aa7dfaf695b
kiran <gmkumar2005@gmail.com>
2020-05-06 01:30:39 +0530

    builder fixed

--- builder.sh
+++ builder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 (./ubuntu-baseimage.sh -v -r registry.container-registry:5000/ &&  
-./ubuntu-nodeimage  -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-baseimage:latest && 
-./node-baseimage -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
+./ubuntu-nodeimage.sh  -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-baseimage:latest && 
+./node-baseimage.sh -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
