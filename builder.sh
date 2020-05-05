0edb4619814a408e09828eac369d34ced54020c3
kiran <gmkumar2005@gmail.com>
2020-05-06 02:03:06 +0530

    update builder

--- builder.sh
+++ builder.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-(./ubuntu-baseimage.sh -v -r registry.container-registry:5000/ &&  
-./ubuntu-nodeimage.sh  -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-baseimage:latest && 
-./node-baseimage.sh -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
+(./node-baseimage.sh -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
