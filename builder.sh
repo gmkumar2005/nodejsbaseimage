1a400335ed551f3a1ccbd28ed94e1f03362a79d1
kiran <gmkumar2005@gmail.com>
2020-05-06 13:46:34 +0530

    All images

--- builder.sh
+++ builder.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(./node-baseimage.sh -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
+(./ubuntu-baseimage.sh -v -r registry.container-registry:5000/ && ./ubuntu-nodeimage  -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-baseimage:latest && ./node-baseimage -v -r registry.container-registry:5000/ -f registry.container-registry:5000/ubuntu-nodeimage:latest)
